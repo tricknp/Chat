b4db8134f26b6823f8d166d8d326ea0783bde104
Patrick Porto <np_patrick@yahoo.com>
2017-11-02 18:22:30 -0200

    Update settings.php

--- _settings/settings.php
+++ _settings/settings.php
@@ -6,7 +6,7 @@
 
 	//connection start
 	$con = mysqli_connect($dbhost, $dbuser, $dbpass) or print(mysql_error());
-	mysqli_select_db($dbname, $con);
+	mysqli_select_db($con, $dbname);
 
 	if (!$con) {
 		echo "Error: Database not found";
