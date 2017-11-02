12c4f35efd2fddc15f9462d1b0119e3660fdb4ed
Patrick Porto <np_patrick@yahoo.com>
2017-11-02 00:17:55 -0200

    Update chat.php

--- _pages/chat.php
+++ _pages/chat.php
@@ -7,8 +7,26 @@
 		<input type="text" name="msg" id="msg" placeholder="your message...">
 		<span>
 			<input type="submit" value="&rang;&rang;&rang;">
+			<input type="hidden" name="env" value="envMsg" id="env">
 		</span>
 	</form>
+
+	<?php 
+		if (isset($_POST['env']) && $_POST['env'] == "envMsg") {
+			$msg = $POST['msg'];
+			$id_for = $_GET['usuario'];
+			$id_of = $currentUser;
+
+			if (empty($msg)) {
+				echo "<code>Enter a message</code>";
+			}else{
+				if (mysqli_query("INSERT INTO mensagens (id_de, id_para, mensagens) VALUES ('$id_of', '$id_for', '$msg')")) {
+				}else{
+					echo "<code> Error: msg not sent.</code>";
+				}
+			}
+		}
+	 ?>
 	<br>
 	
 </div>
