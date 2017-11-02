f45e6fecfa74c09723634d9a21006ae4c0083f04
Patrick Porto <np_patrick@yahoo.com>
2017-11-02 00:18:28 -0200

    Update home.php

--- _pages/home.php
+++ _pages/home.php
@@ -10,31 +10,31 @@
 		<p><input type="password" name="password" id="password" placeholder="********"></p>
 
 		<p><input type="submit" name="enter" value="Enter"></p>
-		<input type="hidden" name="env" value="login">
+		<input type="hidden" name="env" value="login" id="env">
 	</form>
 
 	<?php 
-		if (isset($_POST[env]) && $_POST['env'] == "login") {
+		if (isset($_POST['env']) && $_POST['env'] == "login") {
 			$user = $_POST['user'];
 			$pass = $_POST['password'];
 
 			if (empty($user) || empty($pass)) {
 				echo "<code>Complete all fields</code>";
 			}else{
-				$query = "SELECT * FROM users WHERE usuario = '$user' AND senha = '$pass'";
-				$result = mysql_query($query);
-				$search = mysql_num_rows($result);
-				$line =  mysql_fetch_assoc($result);
+				$query = "SELECT * FROM usuarios WHERE usuario = '$user' AND senha = '$pass'";
+				$result = mysqli_query($query);
+				$search = mysqli_num_rows($result);
+				$line = mysqli_fetch_assoc($result);
 
-				if (search > 0 ) {
+				if ($search > 0 ) {
 					$_SESSION['nome'] = $line['nome'];
 					$_SESSION['usuario'] = $line['usuario'];
-					echo '<meta http-equiv="Refresh" content=1; href="?page=users">';
+					echo '<meta http-equiv="Refresh" content="1; url=?page=user">';
 				}else{
 					echo "<code>Invalid username or password.</code>";
 				}
 			}
 		}
 	 ?>
-
+	
 </div>
