a38f5ea05ec50c28e83fc120df4111f93ed190f8
Patrick Porto <np_patrick@yahoo.com>
2017-11-02 00:18:57 -0200

    Update user.php

--- _pages/user.php
+++ _pages/user.php
@@ -1,9 +1,26 @@
-
 <div id="content">
+	<?php 
+		$slc = mysqli_query("SELECT * FROM usuarios");
+		$acc = mysqli_num_rows($slc);
+
+		if ($acc <= 0) {
+			echo "User not found";
+		}else{
+			while ($row = mysqli_fetch_array($slc)) {
+				$name = $row['nome'];
+				$user = $row['usuario'];
+				$photo = $row['foto'];			
+			}
+		}
+	 ?>
+
 	<tr>
-		<td><img src="" class="foto-user"></td>
-		<td><strong>Username</strong></td>
-		<td><a href="#"></a>Start</td>
+		<td><img src="<?php echo "$photo";?>" class="foto-user"></td>
+		<td><strong><?php echo "$name";?></strong></td>
+		<td><a href="?page=chat&user=<?php echo $user ?>"></a>Start</td>
 	</tr>
 	<hr>
+
+	<?php }}?>
+
 </div>
